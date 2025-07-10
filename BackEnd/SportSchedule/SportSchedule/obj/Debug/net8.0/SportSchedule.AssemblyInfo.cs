42631d0e65b9ff40528c3b73e372689b9253c1a4
thanhdanhk2004 <121719416+thanhdanhk2004@users.noreply.github.com>
2025-07-10 23:50:03 +0700

    Remove tracked sensitive files now ignored

--- BackEnd/SportSchedule/SportSchedule/obj/Debug/net8.0/SportSchedule.AssemblyInfo.cs
+++ BackEnd/SportSchedule/SportSchedule/obj/Debug/net8.0/SportSchedule.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f26f369e6c89c0b02ee1f115a06424f20e9a4be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbbccbbea25e9ff5ff1952d05dae57fa7df9b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
